59b352aa128b54f87b51f316ae8488d8d9a4f8ea
ajuala <kamalustudio@yandex.com>
2023-11-27 06:11:19 +0100

    pg Client query test, attempt 1

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import pg from 'pg';
 export default {
   async fetch(req, env) {
 
-    const pgclient = new pg.Client({connectionString: env.COCKROACH_CONN_STRING});
+    const pgClient = new pg.Client({connectionString: env.COCKROACH_CONN_STRING});
+    await pgClient.connect();
+
+    const res = await pgClient.queru('SELECT CURRENT_TIMESTAMP AS db_date');
+    const dbDate = res.row[0].db_date;
 
     const data = {
       hello: "world",
+      dbDate,
+      serverDate: new Date().toISOString()
     };
 
     const json = JSON.stringify(data, null, 2);
